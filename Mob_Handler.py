2b254db031649d67a5f951e49efba954558e85db
ShawnHillJ <shawnhillj@gmail.com>
2016-02-11 18:32:51 -0800

    Adding next large update

--- Mob_Handler.py
+++ Mob_Handler.py
@@ -1,13 +1,6 @@
 #Mob_Handler
-#Spawns Friendly and Enemy npcs
+#Spawns Friendly and Enemy mobs
 
 import bge
-cont = bge.logic.getCurrentController()
-own = cont.own
-scene = bge.scene.getCurrentScene()
 
-#NPC logic and data will be bundled in logic brick with NPC's object
-def add_npc(entity_name, spawn_location):
-  
-def remove_npc(entity_active_id_val):
-  
+
